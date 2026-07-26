619053dca128855b9bf292105d8b619252d23730
5mohamedasker <5mohamedasker@gmail.com>
2026-07-26 12:47:11 +0300

    Question01

--- App3/Program.cs
+++ App3/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace App3
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
+
+namespace App3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region Question01
+            ////1.Declare string title = "clean code";
+            ////Call title.ToUpper() and store it in a new variable upperTitle.
+            ////Print both title and upperTitle to show that title did not change.
+            //string title = "clean code";
+            //string upperTitle = title.ToUpper();
+            //Console.WriteLine(title);
+            //Console.WriteLine(upperTitle);
+            #endregion
         }
     }
 }
