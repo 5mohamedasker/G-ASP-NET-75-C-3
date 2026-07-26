54775b451d063ee68d7973de3b0be37a2eb7053f
5mohamedasker <5mohamedasker@gmail.com>
2026-07-26 13:30:58 +0300

    Question08

--- App3/Program.cs
+++ App3/Program.cs
@@ -63,6 +63,16 @@ namespace App3
             //int pages = 464;
             //Console.WriteLine(string.Format("Book: {0}, Pages: {1}",title,pages));
             #endregion
+            #region Question08
+            ////8.Given int pages = 464; write an if / else statement 
+            ////that prints "Long Book" if pages is greater than 300
+            ////otherwise prints "Short Book".
+            //int pages = 464;
+            //if (pages > 300)
+            //    Console.WriteLine("Long Book");
+            //else
+            //    Console.WriteLine("Short Book");
+            #endregion
         }
     }
 }
