625bd4f85029246a50c6137ed86a4950e76333c1
5mohamedasker <5mohamedasker@gmail.com>
2026-07-26 13:38:58 +0300

    Question09

--- App3/Program.cs
+++ App3/Program.cs
@@ -73,6 +73,17 @@ namespace App3
             //else
             //    Console.WriteLine("Short Book");
             #endregion
+            #region Question09
+            ////9.Given int pages = 464; and bool isAvailable = true;
+            ////print "You can borrow this book" only if pages is
+            ////greater than 300 and isAvailable is true.Use the && operator.
+            //int pages = 464;
+            //bool isAvailable = true;
+            //if (pages > 300 && isAvailable)
+            //    Console.WriteLine("You can borrow this book");
+            //else
+            //    Console.WriteLine("You can't borrow this book");
+            #endregion
         }
     }
 }
