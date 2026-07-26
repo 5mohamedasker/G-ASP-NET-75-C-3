06a5d408244859841986b38b32b0d9a39122ba42
5mohamedasker <5mohamedasker@gmail.com>
2026-07-26 14:15:27 +0300

    Question013

--- App3/Program.cs
+++ App3/Program.cs
@@ -124,6 +124,17 @@ namespace App3
             //    Console.WriteLine($"{i + 1}. {books[i]}");
             //}
             #endregion
+            #region Question013
+            ////13.Using the same books array
+            ////use a while loop to print every book title.
+            //string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            //int index = 0;
+            //while (index < books.Length)
+            //{
+            //    Console.WriteLine($"{index + 1}. {books[index]}");
+            //    index++;
+            //}
+            #endregion
         }
     }
 }
