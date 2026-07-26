f54d54c2827098cb7b773812f0fe2b7bd6d50f94
5mohamedasker <5mohamedasker@gmail.com>
2026-07-26 13:47:14 +0300

    Question010

--- App3/Program.cs
+++ App3/Program.cs
@@ -84,6 +84,26 @@ namespace App3
             //else
             //    Console.WriteLine("You can't borrow this book");
             #endregion
+            #region Question010
+            ////10.Given string title = "Refactoring";
+            ////write a switch statement that prints
+            ////"Great choice!" if the title is "Clean Code",
+            ////"Nice pick!"  if it's "Refactoring",
+            ////and "Never heard of it" for anything else.
+            //string title = "Refactoring";
+            //switch(title)
+            //{
+            //    case "Clean Code":
+            //        Console.WriteLine("Great choice!");
+            //        break;
+            //    case "Refactoring":
+            //        Console.WriteLine("Nice pick!");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Never heard of it");
+            //        break;
+            //}
+            #endregion
         }
     }
 }
